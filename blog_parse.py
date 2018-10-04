82bc4478c35921656d0c2ef703c2597775c63753
SonGeon <fjen6b@gmail.com>
2018-10-04 16:58:04 +0900

    change parse_content to reduce request & send data minimize for version1 { title, url, content }

--- blog_parse.py
+++ blog_parse.py
@@ -1,34 +1,28 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 import json
 import os
-import articleDateExtractor
+#import articleDateExtractor
 
 blog_data = []
 blog_content = []
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BUFFER_SIZE = 10
+DOC_UPDATE_URL = "http://127.0.0.1:3000/document"
 
-def parse(i):
-    req = requests.get(i['url'])
-    html = req.text
+def parse_content(base_url, page_url, html):
     soup = BeautifulSoup(html, 'html.parser')
     d = {}
-    d['id'] = str(i['id'])
-    d['blog'] = str(i['blog'])
-    d['url'] = str(i['url'])
+#    d['id'] = str(i['id'])
+    d['blog'] = base_url
+    d['url'] = page_url
     d['title'] = str(soup.title.text)
     d['content'] = str(soup.body.text)
-    d["creationDate"] = str(articleDateExtractor.extractArticlePublishedDate(i['url']))
-    d["creationDate"] = d["creationDate"][:10]
-    blog_content.append(d)
+#    d["creationDate"] = str(articleDateExtractor.extractArticlePublishedDate(i['url']))
+#    d["creationDate"] = d["creationDate"][:10]
+    buffered_document_send(d)
 
-
-with open(os.path.join(BASE_DIR, 'blog_list.json'),'r', encoding='utf-8') as json_file:
-    blog_data = json.load(json_file)
-
-for i in blog_data:
-    parse(i)
-
-with open(os.path.join(BASE_DIR, 'blog_content.json'), 'w+', encoding="utf-8") as json_file:
-    json.dump(blog_content, json_file, ensure_ascii=False)
+def buffered_document_send(data):
+    headers = {'Content-Type': 'application/json', 'Accept':'application/json', 'charset':'utf-8'}
+    res = requests.post(DOC_UPDATE_URL, headers=headers, json=[data])
 

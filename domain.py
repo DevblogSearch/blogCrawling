06c4c160e16f6aa566cf772e7076f965f74327e5
beomsoo jeon <rhyme884@gmail.com>
2018-11-13 14:59:21 +0900

    domain.py 수정

--- domain.py
+++ domain.py
@@ -4,8 +4,7 @@ from urllib.parse import urlparse
 # Get domain name (example.com)
 def get_domain_name(url):
     try:
-        results = get_sub_domain_name(url).split('.')
-        return results[-2] + '.' + results[-1]
+        return url
     except:
         return ''
 

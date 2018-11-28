2ff9ba69d586ccc21b8a5dbfdfabb29d7016cc43
beomsoo jeon <rhyme884@gmail.com>
2018-11-28 11:25:03 +0900

    main.py 코드 병합 ( Thread 삭제, 크롤링 동작 변경)

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from queue import Queue
 from spider import Spider
 from domain import *
 from general import *
+from selenium import webdriver
+import sys, os
 
 stream = open('db_sync_blogspot.yml', 'r', encoding="UTF8")
 data = yaml.load(stream)
@@ -11,50 +13,29 @@ NUMBER_OF_THREADS = 1
 # db.yml = 1019 // db_naver_yml = 35 // db_sync_blogspot = 3
 NUMBER_OF_BLOGGERS = 1
 
-# Create worker threads (will die when main exits)
-def create_workers():
-    for _ in range(NUMBER_OF_THREADS):
-        t = threading.Thread(target=work)
-        t.daemon = True
-        t.start()
-
-
-# Do the next job in the queue
-def work():
-    while True:
-        url = queue.get()
-        Spider.crawl_page(threading.current_thread().name, url)
-        queue.task_done()
-
-
-# Each queued link is a new job
-def create_jobs():
-    for link in file_to_set(QUEUE_FILE):
-        queue.put(link)
-    queue.join()
-    crawl()
-
-
-# Check if there are items in the queue, if so crawl them
-def crawl():
-    queued_links = file_to_set(QUEUE_FILE)
-    if len(queued_links) > 0:
-        print(str(len(queued_links)) + ' links in the queue')
-        create_jobs()
-
-
 for person in range(NUMBER_OF_BLOGGERS):
     try:
+        if 'name' not in data[person] or 'blog' not in data[person]:
+            continue
+
         PROJECT_NAME = data[person]['name']
         HOMEPAGE = data[person]['blog']
         DOMAIN_NAME = data[person]['blog']
         Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
-        QUEUE_FILE = 'user\\' + PROJECT_NAME + '/queue.txt'
-        CRAWLED_FILE = 'user\\' + PROJECT_NAME + '/crawled.txt'
-        queue = Queue()
+        QUEUE_FILE = os.path.join('user' , PROJECT_NAME , 'queue.txt')
+        CRAWLED_FILE = os.path.join('user' , PROJECT_NAME , 'crawled.txt')
 
-        create_workers()
-        crawl()
+        while True:
+            queued_links = file_to_set(QUEUE_FILE)
+            if len(queued_links) == 0:
+                break
 
-    except: pass
+            for link in queued_links:
+                Spider.crawl_page(threading.current_thread().name, link)
+
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(str(e))
